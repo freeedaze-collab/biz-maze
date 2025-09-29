b94ea30440b4a96a094e2ad7b23986f272c4721b
freeedaze-collab <freeedaze@gmail.com>
2025-09-29 15:08:42 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { WagmiProvider } from '@/providers/WagmiProvider';
+import { AuthProvider } from '@/hooks/useAuth';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <WagmiProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </WagmiProvider>
   </BrowserRouter>
 );
