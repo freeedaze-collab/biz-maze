093511f4d493a40a63bf4f87ab2a99a0dbad7449
freeedaze-collab <freeedaze@gmail.com>
2025-09-29 13:53:53 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { WagmiProvider } from '@/providers/WagmiProvider';
-import "./index.css";
-import { useScrollToTop } from "./hooks/useScrollToTop";
-
-function AppWithScrollToTop() {
-  useScrollToTop();
-  return <App />;
-}
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
@@ -16,5 +9,4 @@ createRoot(document.getElementById('root')!).render(
       <App />
     </WagmiProvider>
   </BrowserRouter>
-
 );
