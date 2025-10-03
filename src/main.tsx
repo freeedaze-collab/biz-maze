c3c75ee61d8f2e7de1f85146ada364aaa99125e8
freeedaze-collab <freeedaze@gmail.com>
2025-10-03 12:26:09 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,37 +2,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-// wagmi
 import { WagmiProvider } from "wagmi";
 import { wagmiConfig } from "@/config/wagmi";
-
-// react-query
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-// router
-import { HashRouter } from "react-router-dom";
-
-// ★ src/hooks/useAuth.tsx から AuthProvider を import
 import { AuthProvider } from "@/hooks/useAuth";
 
-const isDev = import.meta.env.DEV;
 const queryClient = new QueryClient();
 
-const Root = (
-  <WagmiProvider config={wagmiConfig}>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider> {/* AuthProvider でラップ */}
-        <HashRouter>
-          <App />
-        </HashRouter>
-      </AuthProvider>
-    </QueryClientProvider>
-  </WagmiProvider>
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </AuthProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>
 );
-
-const AppRoot = isDev ? Root : (
-  <React.StrictMode>{Root}</React.StrictMode>
-);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(AppRoot);
