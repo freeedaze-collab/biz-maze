7cc5f749db4abda205e0d4804be7a80b24967db4
freeedaze-collab <freeedaze@gmail.com>
2025-10-09 23:07:47 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": path.resolve(__dirname, "./src")
     },
   },
   server: {
@@ -24,3 +25,5 @@ export default defineConfig({
     strictPort: true,
   },
 })
+
+
